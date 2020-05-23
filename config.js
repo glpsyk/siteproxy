b66b1b2f0f88432eebbf06d6c89fe296d83a069e
glpsyk <65812513+glpsyk@users.noreply.github.com>
2020-05-23 19:21:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'scholar.now.sh',
+    serverName: 'scholar-lshkt7v5m.now.sh',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
